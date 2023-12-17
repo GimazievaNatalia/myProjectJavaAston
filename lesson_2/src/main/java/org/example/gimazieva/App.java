8c2833fc600938703be8e981147b81a7ffe26008
Natalia <imsi88@mail.ru>
2023-12-17 18:43:21 +0400

    lesson 2 commit

--- lesson_2/src/main/java/org/example/gimazieva/App.java
+++ lesson_2/src/main/java/org/example/gimazieva/App.java
@@ -1,13 +1,54 @@
 package org.example.gimazieva;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void printThreeWords() {
+        String[] words = {"Orange", "Banana", "Apple"};
+        for (String word : words) {
+            System.out.println(word);
+        }
+    }
+
+    public static void checkSumSign(){
+        int A = 1;
+        int B = -1;
+        int sum = A + B;
+        if (sum >= 0){
+            System.out.println("Сумма положительная");
+        }
+        else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor(){
+        int  VALUE = 101;
+        if (VALUE <= 0) {
+            System.out.println("Красный");
+        }
+        if (VALUE > 0 && VALUE <= 100) {
+            System.out.println("Желтый");
+        }
+        else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers(){
+        int A = 30;
+        int B = 20;
+        if (A >= B) {
+            System.out.println("a >= b");
+        }
+        else {
+            System.out.println("a < b");
+        }
+    }
+
+    public static void main( String[] args ) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
     }
 }
