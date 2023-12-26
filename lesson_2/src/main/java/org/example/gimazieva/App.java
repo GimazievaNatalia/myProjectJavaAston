a5ce9fb1a75ac6a4f1eae92dcb14266415d1c137
Natalia <imsi88@mail.ru>
2023-12-26 21:15:46 +0400

    change lesson2 App

--- lesson_2/src/main/java/org/example/gimazieva/App.java
+++ lesson_2/src/main/java/org/example/gimazieva/App.java
@@ -1,54 +1,37 @@
 package org.example.gimazieva;
 
-public class App
-{
+public class App {
+
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
     public static void printThreeWords() {
         String[] words = {"Orange", "Banana", "Apple"};
         for (String word : words) {
             System.out.println(word);
         }
     }
-
-    public static void checkSumSign(){
-        int A = 1;
-        int B = -1;
-        int sum = A + B;
-        if (sum >= 0){
-            System.out.println("Сумма положительная");
-        }
-        else {
-            System.out.println("Сумма отрицательная");
-        }
+    public static void checkSumSign() {
+        int a = 1;
+        int b = -1;
+        System.out.println((a + b >= 0) ? "Сумма положительная": "Сумма отрицательная");
     }
-
-    public static void printColor(){
-        int  VALUE = 101;
-        if (VALUE <= 0) {
+    public static void printColor() {
+        int value = 100;
+        if (value > 100) {
+            System.out.println("Зеленый");
+        } else if (value <= 0) {
             System.out.println("Красный");
-        }
-        if (VALUE > 0 && VALUE <= 100) {
+        } else {
             System.out.println("Желтый");
         }
-        else {
-            System.out.println("Зеленый");
-        }
     }
-
-    public static void compareNumbers(){
-        int A = 30;
-        int B = 20;
-        if (A >= B) {
-            System.out.println("a >= b");
-        }
-        else {
-            System.out.println("a < b");
-        }
-    }
-
-    public static void main( String[] args ) {
-        printThreeWords();
-        checkSumSign();
-        printColor();
-        compareNumbers();
+    public static void compareNumbers() {
+        int a = 30;
+        int b = 20;
+        System.out.println((a >= b) ? "a >= b" : "a < b");
     }
 }
